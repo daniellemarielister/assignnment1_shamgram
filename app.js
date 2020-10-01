171ff40a834da1c85ed6293b5a16036ab52cffbd
Danielle Lister <daniellemarielister@gmail.com>
2020-10-01 16:24:48 -0700

    Mostly done assignemnt1

--- app.js
+++ app.js
@@ -1,32 +1,223 @@
-var http = require('http');
-var fs = require('fs');
-var index = fs.readFileSync( 'index.html');
+const express = require('express');
+const app = express();
+const PORT = 3000;
+var http = require('http').createServer(app);
+http.listen(3000, () => {
+    console.log('listening on *:3000');
+  });
 
-var app = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
+var fs = require('fs');
+const { Socket } = require('dgram');
+var index = fs.readFileSync('index.html');
+
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "nodejs",
+  password: "shamgram",
+  database: "shamgram",
+  port: 8889
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
     res.end(index);
 });
 
-var io = require('socket.io').listen(app);
 
-setInterval(function sendTime() {
-    
-    io.emit('time', { time: new Date().toJSON() });
-    console.log( 'EMIT: time');
-    
-}, 10000);
-
-io.on('connection', function(socket) {
-    
-    socket.on('fromClient',function(data){
-   
-        console.log( 'ON: fromClient');
-        
-        socket.emit('fromServer', { message: 'Received message! Returning message!!' });
-        console.log( 'EMIT: fromServers');
-    
-    });
-    
+app.post('/upload', (req, res) => {
+    console.log(req.file);
+    if(req.file) {
+        console.log("Connected!");
+        var query = "INSERT INTO photos (photoFile) VALUES ?";
+        var values = {photoFile: req.file};
+        con.query(query, values, function (err, result) {
+            if (err) throw err;
+            console.log("1 record inserted, ID: " + result.insertId);
+        });
+    }
+    else throw 'error';
 });
 
-app.listen(3000);
+var io = require('socket.io').listen(http);
+
+io.on('connection', function(socket){
+
+    socket.on('getListOfPhotos', function(){
+    con.query("SELECT photoID FROM photos", function (err, result, fields) {
+        if (err) throw err
+        console.log(result);
+        var ids = [];
+        result.forEach(function(row) {
+            ids.push(row.photoID); // figure this out
+         });
+        console.log(ids);
+        socket.emit("receivePhotoIDs", {photoIDs:ids});
+    });
+    });
+    
+    // getting comments for DB
+    socket.on('getCommentsByID', function(data){
+        var commentsArray = [];
+        
+        // make sql querry with photoID
+        var photoID = data.photoID;
+        var query = 'SELECT comment FROM comments WHERE photoID=?'
+        con.query( query, [photoID], function (err, result, fields) {
+            if (err) throw err
+            console.log(result);
+            // put the results into comments array
+            result.forEach(function(row) {
+                commentsArray.push(row.comment);
+                console.log(row.comment);
+                
+            });
+            console.log('asdkjgh'); 
+            socket.emit( 'commentsFromServer', {comments: commentsArray, photoID: photoID});  
+        });        
+             
+    });
+
+    // getting likes from DB
+    socket.on('getLikesByID', function(data){
+        console.log('getting likes');
+        var photoID = data.photoID;
+        var likeArray = [];
+        // make sql querry to get like count from like table by photo id
+        var sql = 'SELECT likeTally FROM likeCounter WHERE photoID = ?';
+        con.query( sql, [photoID], function (err, result, fields) {
+            if (err) throw err
+            console.log('_________________');
+            console.log(result);
+            result.forEach(function(row) {
+                likeArray.push(row.likeTally);
+                console.log(row.likeTally);
+                
+            });
+            console.log(likeArray);
+            socket.emit( 'likesFromServer', {likes: likeArray, photoID: data.photoID});
+            
+        });
+        
+    });
+
+
+
+
+    // uploading a comment
+    socket.on('sendComment', function(data){
+        console.log(data.comment);
+        console.log(data.photoID);
+
+        photoID = data.photoID;
+        comment = data.comment;
+        // take the comment 
+        // don't think this is write we want what was sent as "comment: comment.value" and we want the photo id's value "photoid: photoid"
+        var sql = "INSERT INTO `comments` (`photoID`, `comment`) VALUES ('?', ?)";
+        // // insert into comments table the comment to the photoID 
+        // // ***** does the photo id and comment go into [] or not?
+        con.query( sql, [photoID, comment], function (err, result, fields) {
+            if (err) throw err
+            console.log(result);
+            });
+        socket.emit( 'reloadCommentsFromServer', { photoID: photoID});
+
+    });
+
+    // adding a like
+    socket.on('addLike', function(data){
+        console.log('got like');
+        var photoID = data.photoID;  // or is it .value?
+        console.log(photoID);
+        var likeArray = [];
+
+        var query = "SELECT likeTally FROM likeCounter WHERE = ?";
+        var sql = 'SELECT likeTally FROM likeCounter WHERE photoID = ?';
+        con.query( sql, [photoID], function (err, result, fields) {
+            if (err) throw err
+            console.log('_________________');
+            console.log(result);
+            result.forEach(function(row) {
+                likeArray.push(row.likeTally);
+                console.log(row.likeTally);
+                
+            });
+            var sql2 = 'UPDATE `likeCounter` SET `likeTally`=? WHERE photoID=?';
+            likeArray ++;
+            console.log(likeArray);
+            con.query( sql2, [likeArray, photoID], function (err, result, fields) {
+                if (err) throw err
+                console.log('sucess');
+                console.log(result);
+                    
+                });
+            console.log(likeArray);
+            socket.emit( 'likesFromServer', {likes: likeArray, photoID: data.photoID});
+            
+        });
+    });
+
+    // socket.on("getPhotoIDs", function(data){
+    //     console.log("Photo IDs Requested")
+    //     con.query("SELECT photoID FROM photos", function (err, result, fields) {
+    //         if (err) throw err
+    //         console.log(result);
+    //         var ids = [];
+    //         result.forEach(function(row) {
+    //             ids.push(row.photoID); // figure this out
+    //         });
+    //         console.log(ids);
+    //         socket.emit("receivePhotoIDs", {photoIDs:ids});
+    //     });
+        
+    // });
+
+    socket.on("getPhotoByID", function(data){
+            console.log("photos ID Requested again")
+            var photoID = data.photoID;
+            var sql = 'SELECT photoFile FROM photos WHERE photoID = ?';
+            console.log(photoID);
+            con.query(sql, [photoID], function (err, result) {
+                if (err) throw err;
+                socket.emit ("receivePhoto", {photo: result[0].photoFile, photoID: photoID}); // figure this out
+                console.log("sent photos to the server " + photoID);
+                });
+    });
+
+    // ******************************* get 2nd post *****************************************
+
+    socket.on('getCommentsByID2', function(data){
+        var commentsArray = [];
+        
+        // make sql querry with photoID
+        var photoID = 2;
+        var query = 'SELECT comment FROM comments WHERE photoID=?'
+        con.query( query, [photoID], function (err, result, fields) {
+            if (err) throw err
+            console.log(result);
+            // put the results into comments array
+            result.forEach(function(row) {
+                commentsArray.push(row.comment);
+                console.log(row.comment);
+                
+            });
+            console.log('c'); 
+            socket.emit( 'commentsFromServer2', {comments: commentsArray, photoID: photoID});  
+        });        
+             
+    });
+
+
+        
+    });
+
+
+    
---
+++ test.js
@@ -0,0 +1,43 @@
+var array = [1, 2, 6, 5, 4];
+
+// function test_func(izzy, idx){
+//     console.log(idx);
+//     console.log(izzy*izzy);
+// }
+
+//array.forEach(test_func);
+array.forEach(function(row){
+    console.log(row);
+
+});
+
+// result.forEach(function(row) {
+//     ids.push(row.photoID); // figure this out
+// });
+
+// var obj_example = {name:"Izzy", age:12, ahahah:test_func};
+
+// console.log(obj_example.name);
+// array.forEach(obj_example.ahahah);
+// console.log("-----")
+// obj_example.ahahah(9, 5);
+
+// console.log('------------------------');
+// var test_data = {some_number:1123, another_number:8213};
+// console.log(test_data.some_number);
+// console.log('------------------------');
+
+// function f(x){
+//     console.log(x);
+//     return x**2;
+// };
+// var value = 3;
+// var a_squared_value = f(value);
+// console.log(a_squared_value);
+
+// . acccess objects
+// () run fuctions
+// {} Defines scope
+// functions associated with objects are acalled methods
+// object.method
+// Arrays are just special types of objects
