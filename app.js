a86d806c6382e87f831bd37a446fc98b6871687a
Danielle Lister <daniellemarielister@gmail.com>
2020-10-02 17:50:45 -0700

    Shamgram v1

--- app.js
+++ app.js
@@ -9,10 +9,9 @@ http.listen(3000, () => {
 var fs = require('fs');
 const { Socket } = require('dgram');
 var index = fs.readFileSync('index.html');
-
-
 var mysql = require('mysql');
 
+// connect to mysql db
 var con = mysql.createConnection({
   host: "localhost",
   user: "nodejs",
@@ -32,7 +31,6 @@ app.get('/', (req, res) => {
     res.end(index);
 });
 
-
 app.post('/upload', (req, res) => {
     console.log(req.file);
     if(req.file) {
@@ -50,43 +48,36 @@ app.post('/upload', (req, res) => {
 var io = require('socket.io').listen(http);
 
 io.on('connection', function(socket){
-
+    // gets lists of all photo ids from db and sends them back in a array
     socket.on('getListOfPhotos', function(){
     con.query("SELECT photoID FROM photos", function (err, result, fields) {
         if (err) throw err
-        console.log(result);
         var ids = [];
         result.forEach(function(row) {
-            ids.push(row.photoID); // figure this out
+            ids.push(row.photoID);
          });
-        console.log(ids);
         socket.emit("receivePhotoIDs", {photoIDs:ids});
     });
     });
     
-    // getting comments for DB
+    // getting comments for DB by photo id and sends them back in a array
     socket.on('getCommentsByID', function(data){
         var commentsArray = [];
-        
-        // make sql querry with photoID
         var photoID = data.photoID;
         var query = 'SELECT comment FROM comments WHERE photoID=?'
         con.query( query, [photoID], function (err, result, fields) {
             if (err) throw err
-            console.log(result);
-            // put the results into comments array
+            // puts just the comment value the results into comments array
             result.forEach(function(row) {
                 commentsArray.push(row.comment);
-                console.log(row.comment);
-                
             });
-            console.log('asdkjgh'); 
+            // sends to client
             socket.emit( 'commentsFromServer', {comments: commentsArray, photoID: photoID});  
         });        
              
     });
 
-    // getting likes from DB
+    // getting the likes from DB by photo id 
     socket.on('getLikesByID', function(data){
         console.log('getting likes');
         var photoID = data.photoID;
@@ -95,91 +86,55 @@ io.on('connection', function(socket){
         var sql = 'SELECT likeTally FROM likeCounter WHERE photoID = ?';
         con.query( sql, [photoID], function (err, result, fields) {
             if (err) throw err
-            console.log('_________________');
-            console.log(result);
             result.forEach(function(row) {
                 likeArray.push(row.likeTally);
-                console.log(row.likeTally);
-                
             });
-            console.log(likeArray);
             socket.emit( 'likesFromServer', {likes: likeArray, photoID: data.photoID});
             
         });
-        
     });
 
-
-
-
-    // uploading a comment
+    // uploading a comment to db based on photoid
     socket.on('sendComment', function(data){
-        console.log(data.comment);
-        console.log(data.photoID);
-
         photoID = data.photoID;
+        // comment from users input
         comment = data.comment;
-        // take the comment 
-        // don't think this is write we want what was sent as "comment: comment.value" and we want the photo id's value "photoid: photoid"
         var sql = "INSERT INTO `comments` (`photoID`, `comment`) VALUES ('?', ?)";
-        // // insert into comments table the comment to the photoID 
-        // // ***** does the photo id and comment go into [] or not?
+        // inserts comment into comments by photoID 
         con.query( sql, [photoID, comment], function (err, result, fields) {
             if (err) throw err
             console.log(result);
             });
+        // after uploading will pass back the photo id to reload the comments
         socket.emit( 'reloadCommentsFromServer', { photoID: photoID});
-
     });
 
     // adding a like
     socket.on('addLike', function(data){
-        console.log('got like');
-        var photoID = data.photoID;  // or is it .value?
+        var photoID = data.photoID;
         console.log(photoID);
         var likeArray = [];
-
-        var query = "SELECT likeTally FROM likeCounter WHERE = ?";
+        // first query gets the total like by photo id
         var sql = 'SELECT likeTally FROM likeCounter WHERE photoID = ?';
         con.query( sql, [photoID], function (err, result, fields) {
             if (err) throw err
-            console.log('_________________');
-            console.log(result);
             result.forEach(function(row) {
+                // gets the number of likes and puts in likeArray
                 likeArray.push(row.likeTally);
-                console.log(row.likeTally);
-                
             });
+            // second query updates the like total with the new value
             var sql2 = 'UPDATE `likeCounter` SET `likeTally`=? WHERE photoID=?';
+            // adds 1 to likeArray
             likeArray ++;
-            console.log(likeArray);
             con.query( sql2, [likeArray, photoID], function (err, result, fields) {
                 if (err) throw err
-                console.log('sucess');
-                console.log(result);
-                    
                 });
-            console.log(likeArray);
+            // sends the like total and photo id back to client
             socket.emit( 'likesFromServer', {likes: likeArray, photoID: data.photoID});
-            
         });
     });
 
-    // socket.on("getPhotoIDs", function(data){
-    //     console.log("Photo IDs Requested")
-    //     con.query("SELECT photoID FROM photos", function (err, result, fields) {
-    //         if (err) throw err
-    //         console.log(result);
-    //         var ids = [];
-    //         result.forEach(function(row) {
-    //             ids.push(row.photoID); // figure this out
-    //         });
-    //         console.log(ids);
-    //         socket.emit("receivePhotoIDs", {photoIDs:ids});
-    //     });
-        
-    // });
-
+    // retreives photo file from db based on photo id
     socket.on("getPhotoByID", function(data){
             console.log("photos ID Requested again")
             var photoID = data.photoID;
@@ -187,37 +142,10 @@ io.on('connection', function(socket){
             console.log(photoID);
             con.query(sql, [photoID], function (err, result) {
                 if (err) throw err;
-                socket.emit ("receivePhoto", {photo: result[0].photoFile, photoID: photoID}); // figure this out
-                console.log("sent photos to the server " + photoID);
+                socket.emit ("receivePhoto", {photo: result[0].photoFile, photoID: photoID}); // results is 0 because it's the first photo always in the string
                 });
     });
-
-    // ******************************* get 2nd post *****************************************
-
-    socket.on('getCommentsByID2', function(data){
-        var commentsArray = [];
-        
-        // make sql querry with photoID
-        var photoID = 2;
-        var query = 'SELECT comment FROM comments WHERE photoID=?'
-        con.query( query, [photoID], function (err, result, fields) {
-            if (err) throw err
-            console.log(result);
-            // put the results into comments array
-            result.forEach(function(row) {
-                commentsArray.push(row.comment);
-                console.log(row.comment);
-                
-            });
-            console.log('c'); 
-            socket.emit( 'commentsFromServer2', {comments: commentsArray, photoID: photoID});  
-        });        
-             
-    });
-
-
-        
-    });
+});
 
 
     
